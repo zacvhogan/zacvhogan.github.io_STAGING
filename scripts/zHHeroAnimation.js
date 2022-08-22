e94b2c4fa8cb2b9be52f62b53b164ae92bb0f5a9
Zac Hogan <zachoganpersonal@gmail.com>
2022-08-22 12:37:55 +1200

    Progress on Hero Image Interaction

--- scripts/zHHeroAnimation.js
+++ scripts/zHHeroAnimation.js
@@ -1,5 +1,6 @@
 let clickCount = 0;
 let clickMax = 6;
+let popupLifespan = 3000;
 
 let popupList = [
   "this does nothing",
@@ -8,17 +9,15 @@ let popupList = [
   "are you for real?",
   "I need you to chill out",
   "NOW YOU'VE DONE IT"
-]
+];
 
 
 $(document).ready(function(){
- 
 
-
-  $(".image-hero").on("click", function(){
+  $(".header-logo__image").on("click", function(){
     if (clickCount < clickMax){
       showPopup();
-      clickCount++;
+      clickCount++;      
     }
     else{
       finalAnimation();
@@ -33,6 +32,25 @@ function showPopup(){
 
   console.log(popupList[clickCount]);
 
+  let popup = "<div class = 'header-logo__image__popup'>" + popupList[clickCount] + "</div>";
+  let slideAmount;
+
+  if (clickCount % 2 === 0) {
+    $("#header-logo__popup-area-1").prepend(popup);
+  $(".header-logo__image__popup").animate({
+    opacity: 0,    
+  }, 3000);
+  }
+  else {
+    $("#header-logo__popup-area-2").prepend(popup);
+  $(".header-logo__image__popup").animate({
+    opacity: 0,    
+  }, 3000);
+  }
+
+  
+
+  
 }
 
 
