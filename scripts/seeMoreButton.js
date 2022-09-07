a52b2dfe57dafc7b8c4f594453473ef1af3c038d
Zac Hogan <zachoganpersonal@gmail.com>
2022-09-07 15:35:24 +1200

    feat: progress on seeMoreButton.js, basic functionality working. Minor bugs, to fix.

--- scripts/seeMoreButton.js
+++ scripts/seeMoreButton.js
@@ -13,48 +13,46 @@ buttonList.forEach((button) => button.addEventListener("click", buttonClick.bind
 
 function buttonClick() { 
 
-  // For each button, generate selector for extended content <p>
-  // Using parent element className and identifying suffix
-  let extendedContentSelector = this.closest("div").className + "__extended";
+  // For each button, generate selector for extended content <p> paragraph
+  // Using the className of the parent <div> and an identifying suffix.
+  // TODO: Probably shouldn't be doing this on each click. Refactor to do this once per page load instead.
+  let extendedContentSelector = "." + this.closest("div").className + "__extended";
+  let extendedContent = document.querySelector(extendedContentSelector);  
 
-
-
+  // Get current visibility status of the extended content
+  // Do this once per click to ensure visiblity status always up to date
   let extendedContentCSS = window.getComputedStyle(extendedContent);
   let extendedContentState = extendedContentCSS.getPropertyValue('visibility');
-  console.log(extendedContentState);
-  console.log(ev);
 
+ 
+
+  // Toggle content depending on visibility
   if (extendedContentState == "visible"){
-    console.log("time to hide");
+    toggleContent(extendedContent, "hide")
   }
   else if (extendedContentState == "hidden"){
-    console.log("Time to show");
+    toggleContent(extendedContent, "show")
   }
-
-
-  // if (extendedContent.css{
-  //   console.log(extendedVisible);
-  //   // showContent("normal", extendedContent, maxHeight);
-  //   extendedVisible = true;
-  //   // button.innerHTML = "Show Less";
-  // }
-  // else if (extendedVisible === true){
-  //   console.log(extendedVisible);
-  //   // showContent("reverse", extendedContent, maxHeight);
-  //   extendedVisible = false;
-  //   // button.innerHTML = "Show More";
-  // }  
 }
 
 
-function showContent(animDirection, extendedContent, maxHeight){
+function toggleContent(extendedContent, action){  
 
   let heightDelay;
   let visibilityDelay;
+  let animDirection;
   
-  // TODO - convoluted solution, revise.
+  // Maximum height of extendable content. Calculated each button click to ensure up to date with
+  //  document and viewport dimensions.
+  let maxHeight = extendedContent.scrollHeight + "px";
+
+  // If the action is to show, set CSS animation to play forwards. Else, reverse the animation.
+  action == "show"? animDirection = "normal" : animDirection = "reverse";
+  
+  // Set animation timings.
   // If we're making the section visible, grow section FIRST then show content
   // Otherwise, hide content first then shrink section
+  // TODO: convoluted solution, revise?
   if (animDirection == "normal"){
     heightDelay = 0;
     visibilityDelay = 300;
@@ -65,7 +63,7 @@ function showContent(animDirection, extendedContent, maxHeight){
     visibilityDelay = 0;
   }
 
-  // Animation keys and options for height and visibilty 
+  // Set animation keyframes and options
   let animHeightKeys = [
     {
       height: "0px"        
@@ -81,7 +79,7 @@ function showContent(animDirection, extendedContent, maxHeight){
     easing: "ease",
     direction: animDirection,
     delay: heightDelay,
-    visibility: visibile
+    visibility: "visibile"
   };
 
   let animVisibilityKeys = [
@@ -101,23 +99,20 @@ function showContent(animDirection, extendedContent, maxHeight){
     fill: "forwards",
     easing: "ease",
     direction: animDirection,
-    visibility: hidden
+    visibility: "hidden"
   };
 
-  // Do the animation
+  // Run the animations for height and visibility
   extendedContent.animate(animHeightKeys, animHeightOptions);
   extendedContent.animate(animVisibilityKeys, animVisibilityOptions);
+
+// TODO: update height of extendedContent on window resize to retain good formatting
+// TODO: Change button text on click
+// TODO: Set height initially to 0
+
 
 }
 
 
 
-
-/*
-On click
-- Animate to be taller, then fade in contents
-- Change button text to Less info (arrow up)
-
-
-*/
 
