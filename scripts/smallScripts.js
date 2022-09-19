ff07a246869c94292ac7c1067fd4d2cd3538debc
Zac Hogan <zachoganpersonal@gmail.com>
2022-09-19 18:03:20 +1200

    feat wip: fine tuning formatting of art section

--- scripts/smallScripts.js
+++ scripts/smallScripts.js
@@ -1,10 +1,12 @@
 // Open all hyperlinks in new tab
-// Find all <a> and set target = "_blank"
+// Find all <a> that are NOT for in-page navigation and set target = "_blank"
 
 let links = document.querySelectorAll("a");
 
 links.forEach((link) => {
-  link.setAttribute("target", "_blank");
+  if (!link.classList.contains("button")){  
+    link.setAttribute("target", "_blank");
+  }
 });
 
 
