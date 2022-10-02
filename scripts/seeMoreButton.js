19687998ea430fe289f137750dc5ddc1b91cf137
Zac Hogan <zachoganpersonal@gmail.com>
2022-10-02 22:34:47 +1300

    fix wip: for seeMoreButton.js begin moving animation logic out of buttonClick event

--- scripts/seeMoreButton.js
+++ scripts/seeMoreButton.js
@@ -1,61 +1,69 @@
+/*
+TODO: Rework logic to move animation definition object and variables
+this object references OUT of the buttonClick function
+
+Use a window.resize event to keep maxHeight up to date
+Ensure expandableContent is generated every launch - perhaps as a property of the button object?
+*/
+
+
+
+
 // Find all elements in DOM with the see-more-button class, compile into array.
 let buttonList = document.querySelectorAll(".see-more-button");
 
-// For each button add property 'activated'
-buttonList.forEach((button) => button.activated = false);
-
 // For each button add event listener "click".
 buttonList.forEach((button) => button.addEventListener("click", buttonClick.bind(button)));
+//let expandableContent = document.querySelector("." + this.closest("div").className + "__extended");  
+
+
 
 function buttonClick() {  
-  // For each button, generate selector for extended content div using the className of the parent <div> and an identifying suffix. 
-  // THIS references current button.
-  // TODO: Performance: maybe unwise to do this on EVERY button click? Try on page load.  
-  let expandableContent = document.querySelector("." + this.closest("div").className + "__extended");  
-  let button = this;
-  let buttonIcon = this.firstChild;
 
-  // Set max height of expandable container to fit current contents
+
+  let expandableContent = document.querySelector(".web-portfolio__extended")
   let maxHeight = expandableContent.scrollHeight + "px";  
-  console.log(maxHeight);
-  console.log(this.activated);
-
-  // Set up anime.js animation
-  // Initial values set in styles.css, apart from height which uses a [to, from] range
-  // Do not replace the to-from range with a single to value - this breaks anime.js
+    console.log(maxHeight);
+  
   let hideShowAnim = anime({
     targets: expandableContent,    
     height: [30, maxHeight],   
     "-webkit-mask-image": "linear-gradient(to bottom, black, rgba(0, 0, 0, 1))",
     duration: 250,
-    easing: 'easeInQuad'
+    easing: 'easeInQuad',
+    complete: function(anim) {
+      anim.direction = (anim.direction == "normal") ? "reverse" :  "normal";
+    }
   });
+
+
+  // For each button, generate selector for extended content div using the className of the parent <div> and an identifying suffix. 
+  // THIS references current button.
+  // TODO: Performance: maybe unwise to do this on EVERY button click? Try on page load.  
+ 
+  let button = this;
+  let buttonIcon = this.firstChild;
+
+  // Set max height of expandable container to fit current contents
+  
+ 
+
+  // Set up anime.js animation
+  // Initial values set in styles.css, apart from height which uses a [to, from] range
+  // Do not replace the to-from range with a single to value - this breaks anime.js
+ 
 
   // TODO: The below code plays the animation forwards smoothly, but reverts instantly when
   // it should smoothly reverse animation. Why?
   // Have tried checking whether hideShowAnim.began is true before setting reverse - no luck.
 
-  if (button.activated == false){
-    // Animate
-    hideShowAnim.play();
+
+  // Animate
+  hideShowAnim.restart();
     // Change button icon
-    buttonIcon.classList.remove("fa-chevron-down");
-    buttonIcon.classList.add("fa-chevron-up");
+    // TODO: Animate button
     // Change activated status
-    button.activated = true;
-
-  }
-  else if (button.activated == true) {
-
-    hideShowAnim.play();  
-    hideShowAnim.reverse();
-
-    buttonIcon.classList.remove("fa-chevron-up");
-    buttonIcon.classList.add("fa-chevron-down");
-
-    button.activated = false;
-  }
-  
+   
 }
 
 
